5a6b5948bd52ba7ff6bd2adb3835b9dabab5bfc9
khaeghar <sergiodelgadocastellanos@gmail.com>
2016-09-14 12:41:48 +0200

    Minor changes

--- auto.sh
+++ auto.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm /home/sergio/Toradocu/Toradocu-test/TestArithmetics.json
+rm /home/sergio/Toradocu/TestPlural.json
 
 /usr/bin/java -jar /home/sergio/Repositories/toradocu-dev/build/libs/toradocu-dev-1.0-devel-all.jar \
---target-class com.TestArithmetics \
+--target-class com.TestPlural \
 --source-dir /home/sergio/Repositories/toradocu-testing/src \
 --class-dir /home/sergio/Repositories/toradocu-testing/bin \
 --oracle-generation false \
---condition-translator-output /home/sergio/Toradocu/TestArithmetics.json
+--condition-translator-output /home/sergio/Toradocu/TestPlural.json
