b7f5b50fca36f786677159917f62093f5b8763c3
khaeghar <sergiodelgadocastellanos@gmail.com>
2016-09-19 10:05:54 +0200

    New script model

--- auto.sh
+++ auto.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-rm /home/sergio/Toradocu/TestSingularPlural.json
+CLASS=DescriptiveStatistics
+FOLDERDIR=/home/sergio/Repositories/commons-math3-3.6.1-src
+PACKAGE=org.apache.commons.math3.stat.descriptive
+
+
+rm /home/sergio/Toradocu/$CLASS.json
 
 /usr/bin/java -jar /home/sergio/Repositories/toradocu-dev/build/libs/toradocu-dev-1.0-devel-all.jar \
---target-class com.TestSingularPlural \
---source-dir /home/sergio/Repositories/toradocu-testing/src \
---class-dir /home/sergio/Repositories/toradocu-testing/bin \
+--target-class $PACKAGE.$CLASS \
+--source-dir $FOLDERDIR/src/main/java \
+--class-dir $FOLDERDIR/target/classes \
 --oracle-generation false \
---condition-translator-output /home/sergio/Toradocu/TestSingularPlural.json
+--condition-translator-output /home/sergio/Toradocu/$CLASS.json
